325384bb29df61e4d01cc06927b6371b32e6a385
Andrey Elmanov <andrey.e91@mail.ru>
2022-04-06 02:25:51 +0700

    Большое ДЗ 5. React. Роутинг и контекст

--- src/components/InfoBox/index.jsx
+++ src/components/InfoBox/index.jsx
@@ -1,19 +1,16 @@
 import React from 'react';
 import "./styles.css"
-import Button from '@mui/material/Button';
+import CreateModal from '../CreatePostModal';
 
 
-export const InfoBox = () => {
-    const handleClickCreatePost = function (e) {
-        e.preventDefault();
-        console.log("Есть контакт");
-    }
+export const InfoBox = ({setPosts}) => {
+
  return (
         <div className="info">
             <div className='info_about'>
                 <h3 className='welcome'>Добро пожаловать</h3>
             </div>
-            <Button variant="contained" onClick={handleClickCreatePost}>Разместить пост</Button>
+            <CreateModal setPosts={setPosts}/>
         </div>
  )
 }
